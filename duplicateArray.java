16171c5b830681ab5262053f2bf3563919fd1b89
Praveen1518 <123150901+PraveenOffsider@users.noreply.github.com>
2023-04-04 16:45:42 +0530

    Update duplicateArray.java

--- duplicateArray.java
+++ duplicateArray.java
@@ -5,11 +5,11 @@ public class duplicateArray {
 
         int[] arr = {14, 12, 13, 11, 15, 14, 18, 16, 17, 19, 18, 17, 20};
 
-        // get the length of the array
+
         int len = arr.length;
-        // declare an int variable named count
+
         int count;
-        // iterate from 0 to the array length-1 (outer loop starts here)
+
 
         for (int i = 0; i < len - 1; i++) {
             count = 0;
